3549b20af08a55b5bc2559061121a37db5adf1e1
Gabriel Amaral <gabriel.vidal.amaral@gmail.com>
2023-06-28 11:57:52 -0600

    some styling, and adding the Search Controller

--- angular/index.js
+++ angular/index.js
@@ -8,20 +8,25 @@ var app = angular.module('weatherApp', []);
 app.controller('WeatherController', function($scope, $http) {
     $scope.weather = {};
     //Getting api
-    $http.get('https://api.open-meteo.com/v1/forecast?latitude=40&longitude=-111&daily=temperature_2m_max,temperature_2m_min,sunrise,sunset,uv_index_max,precipitation_sum,windspeed_10m_max&current_weather=true&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&timezone=GMT')
+    $http.get('https://wttr.in/?format=j1')
     
     .then(function(response) {
         //Setting the weather scope to the data
         $scope.weather = response.data;
 
-        //Converting a portion of the data from a number to a readable string
-        if($scope.weather.current_weather.is_day == 1){
-          $scope.weather.current_weather.is_day = "Day"
-        }
-        else{
-          $scope.weather.current_weather.is_day = "Night"
-        }
+        $scope.location = $scope.weather.nearest_area[0].areaName[0].value;
+        $scope.temp_F = $scope.weather.current_condition[0].temp_F;
+        $scope.date = new Date().toUTCString();
+        
         
         console.log($scope.weather)
       });
+});
+
+app.controller('SearchController', function ($scope, $http) {
+
+    function searchLocation() {
+        console.log('ur trash')
+    }
+
   });
