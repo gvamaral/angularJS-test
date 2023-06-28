134201032bb1ff0d66858b8c7294ea0fe1f7b77e
Gabriel Amaral <gabriel.vidal.amaral@gmail.com>
2023-06-28 11:04:27 -0600

    idk

--- angular/index.js
+++ angular/index.js
@@ -7,7 +7,6 @@ var app = angular.module('weatherApp', []);
 
 app.controller('WeatherController', function($scope, $http) {
     $scope.weather = {};
-    $scope.gg = 'Your location!'
     //Getting api
     $http.get('https://api.open-meteo.com/v1/forecast?latitude=40&longitude=-111&daily=temperature_2m_max,temperature_2m_min,sunrise,sunset,uv_index_max,precipitation_sum,windspeed_10m_max&current_weather=true&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&timezone=GMT')
     
