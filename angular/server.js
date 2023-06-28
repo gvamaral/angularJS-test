4d6b88a4ab6de9d90358868239ff95a10a41d89b
John Baird <john.e.baird5@gmail.com>
2023-06-28 11:23:01 -0600

    changed server to express

--- angular/server.js
+++ angular/server.js
@@ -1,43 +1,38 @@
-const http = require('http');
+const express = require('express');
+const app = express();
 const fs = require('fs');
 
-const server = http.createServer((req, res) => {
-  if (req.url === '/') {
-    fs.readFile('index.html', 'utf8', (err, data) => {
-      if (err) {
-        res.writeHead(500, { 'Content-Type': 'text/plain' });
-        res.end('Internal Server Error');
-      } else {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.end(data);
-      }
-    });
-    } 
-  else if (req.url === '/index.js') {
-    fs.readFile('index.js', 'utf8', (err, data) => {
-      if (err) {
-        res.writeHead(500, { 'Content-Type': 'text/plain' });
-        res.end('Internal Server Error');
-      } else {
-        res.writeHead(200, { 'Content-Type': 'text/javascript' });
-        res.end(data);
-      }
-    });
-  }
-  else if (req.url === '/index.css') {
-    fs.readFile('index.css', 'utf8', (err, data) => {
-      if (err) {
-        res.writeHead(500, { 'Content-Type': 'text/plain' });
-        res.end('Internal Server Error');
-      } else {
-        res.writeHead(200, { 'Content-Type': 'stylesheet' });
-        res.end(data);
-      }
-    });
-  }
+app.get('/', (req, res) => {
+  fs.readFile('index.html', 'utf8', (err, data) => {
+    if (err) {
+      res.status(500).send('Internal Server Error');
+    } else {
+      res.status(200).type('html').send(data);
+    }
+  });
+});
+
+app.get('/index.js', (req, res) => {
+  fs.readFile('index.js', 'utf8', (err, data) => {
+    if (err) {
+      res.status(500).send('Internal Server Error');
+    } else {
+      res.status(200).type('js').send(data);
+    }
+  });
+});
+
+app.get('/index.css', (req, res) => {
+  fs.readFile('index.css', (err, data) => {
+    if (err) {
+      res.status(500).send('Internal Server Error');
+    } else {
+      res.status(200).type('css').send(data);
+    }
+  });
 });
 
 const port = 3000;
-server.listen(port, () => {
+app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
