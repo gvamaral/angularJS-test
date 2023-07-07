bb0b48bc78a034966a90e7480f2a3af91afcf7fb
Gabriel Amaral <gabrielamaral@MacBookAir-gabriel.home.lcamaral.com>
2023-07-07 10:20:37 -0600

    humidity icon added

--- angular/index.js
+++ angular/index.js
@@ -83,9 +83,9 @@ app.controller('WeatherController', function($scope, $http) {
     $scope.city = '';
     $scope.SorM = 'sun';
     //Getting api
-    
+
     $http.get(`https://wttr.in/?format=j1`)
-    
+
     .then(function(response) {
         //Setting the weather scope to the data
         $scope.weather = response.data;
@@ -93,15 +93,15 @@ app.controller('WeatherController', function($scope, $http) {
         $scope.location = $scope.weather.nearest_area[0].areaName[0].value;
         $scope.temp_F = $scope.weather.current_condition[0].temp_F;
         $scope.date = $scope.weather.current_condition[0].localObsDateTime;
-        
-        
+
+
         console.log($scope.weather)
     });
 
 
     $scope.searchLocation = () => {
         let url = `https://wttr.in/${$scope.city}, ${$scope.state}?format=j1`;
-   
+
         $http.get(url)
             .then(function (res) {
                 $scope.weather = res.data
@@ -164,8 +164,6 @@ app.controller('WeatherController', function($scope, $http) {
 
 
 //app.controller('SearchController', function ($scope, $http) {
-      
-      
 //      $scope.searchLocation = () => {
 //          let url = `https://wttr.in/${$scope.city}?format=j1`
 //          $http.get(url)
@@ -176,7 +174,5 @@ app.controller('WeatherController', function($scope, $http) {
 //          //   //Edit weather scope
 //          //Error where it doesn't pass as a json object
 //          })
-          
 //      }
-
 //  });
