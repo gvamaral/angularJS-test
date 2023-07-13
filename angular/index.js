8c199c444eaa5c79977174ad2d1de156bd05c3db
Brooke Strickland <brooke.Strickland@namify.com>
2023-07-13 10:49:16 -0600

    shrinking added. working on animation

--- angular/index.js
+++ angular/index.js
@@ -135,7 +135,7 @@ app.controller('WeatherController', function($scope, $http) {
             searchButton.css('color', 'var(--color-white)');
             offCanvas.css('background-color', 'var(--color-black)');
             offCanvas.css('color', 'var(--color-white)');
-            $scope.myStyle = '{--c: var(--color-orange)}'
+            $scope.myStyle = '{--c: var(--color-orange)}';
         }
         else if ($scope.SorM === 'moon') {
             $scope.SorM = 'sun'
@@ -148,7 +148,8 @@ app.controller('WeatherController', function($scope, $http) {
             searchButton.css('color', 'var(--color-black)');
             offCanvas.css('background-color', 'var(--color-white)');
             offCanvas.css('color', 'var(--color-black)');
-            $scope.myStyle = '{--c: var(--color-purple)}'
+            $scope.myStyle = '{--c: var(--color-purple)}';
+
 
         }
     }
@@ -157,13 +158,36 @@ app.controller('WeatherController', function($scope, $http) {
         let weatherNow = angular.element(document.querySelector('.weatherNow'));
         let weatherCard = angular.element(document.querySelector('.weatherCard'));
         let hourCards = angular.element(document.querySelector('.hourCards'));
+        let shrinkText = angular.element(document.querySelector('#shrinkText'));
+        let textChange = angular.element(document.querySelectorAll('.textChange'));
+        let tempChange = angular.element(document.querySelectorAll('.tempChange'));
+        let locationChange = angular.element(document.querySelectorAll('.locationChange'));
 
         if ($scope.showHourWeather === false) {
             $scope.showHourWeather = true;
 
+            //animation
+            shrinkText.css('animation-duration', '1.5s');
+            shrinkText.css('animation-name', 'shrinkSlide');
+            //
+
+
+            shrinkText.css('position', 'absolute');
+            shrinkText.css('top', '19%');
+            shrinkText.css('padding', '0%');
+
+
+            textChange.css('font-size', '15px');
+            textChange.css('text-align', 'center');
+
+            tempChange.css('font-size', '65px');
+            tempChange.css('text-align', 'center');
+
+            locationChange.css('font-size', '22px');
+            locationChange.css('text-align', 'center');
+
         }
         else if ($scope.showHourWeather === true) {
-            $scope.showHourWeather = false;
         }
         else {
             console.error('Something happened on displayHourlyWeather');
